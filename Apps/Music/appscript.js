9f30b4f8abe9a697bf48b0bf3cc489140eb1f5db
Onisha Juhl <onishaj@icloud.com>
2022-03-30 02:14:27 +1300

    UI now resembles Lomiri (unity 8), Loads userdata from a file and has resizable windows

---
+++ Apps/Music/appscript.js
@@ -0,0 +1,4 @@
+function init() {
+	$("#MusicBody").load('/users/template/Apps/Music/screens/artists.html.html');
+}
+init();
--- system/js/main.js
+++ system/js/main.js
@@ -1,45 +1,13 @@
 let temp
 let RunningApps = []
 let History = []
-let UserSettings =[]
+let UserSettings = []
 let CurrentUserName = "template"
+let Active = "Desktop"
 
 function init() {
   console.log("Init Triggered");
   console.log("Loading " + CurrentUserName + " User Data");
-  LoadUserData()
-
-  new Sortable(AppList, {
-    animation: 300,
-    direction: 'horizontal',
-    ghostClass: "DockAppIconGhost"
-  });
-
-  new Sortable(Windows, {
-    animation: 300,
-    direction: 'horizontal',
-    ghostClass: 'ghost'
-  });
-  new Sortable(System, {
-    animation: 300,
-    direction: 'horizontal',
-    ghostClass: "DockAppIconGhost"
-  });
-
-
-  SetWallpaper(UserSettings.Wallpaper.Wallpaper, UserSettings.Wallpaper.Variant, UserSettings.Wallpaper.Ext)
-  displayPinnedList();
-  displayAppList("AllApps");
-  displaySystem();
-
-  if (UserSettings.Sounds.Login == true) {
-    var audio = new Audio('/system/sounds/Login.mp3');
-    audio.play();
-  }
-
-  console.log("Init Complete");
-}
-function LoadUserData() {
   var PathToSettings = "/users/" + CurrentUserName + "/data/User.json";
   var jsonFile = new XMLHttpRequest();
   jsonFile.open("GET", PathToSettings, true);
@@ -53,7 +21,21 @@ function LoadUserData() {
 
   console.log("User Data:");
   console.log(UserSettings)
+
+  SetWallpaper(UserSettings.Wallpaper.Wallpaper, UserSettings.Wallpaper.Variant, UserSettings.Wallpaper.Ext)
+  displayPinnedList();
+  displaySystem();
+
+  if (UserSettings.Sounds.Login == true) {
+    var audio = new Audio('/system/sounds/Login.mp3');
+    audio.play();
+  }
+  console.log("Init Complete");
 }
+
+// function LoadUserData() {
+
+// }
 function SetWallpaper(selection, variant, ext) {
   if (selection != null) {
     UserSettings.Wallpaper.Wallpaper = selection
@@ -71,41 +53,91 @@ function SetWallpaper(selection, variant, ext) {
 
 function AdjustWindowTiling() {
   // if (RunningApps.length == 1) {
-  //   $("#Windows").css("grid-template-columns", "100%")
+  //   $("#Desktop").css("grid-template-columns", "100%")
   //   AppMode = "Full"
   // }
   // if (RunningApps.length == 2) {
-  //   $("#Windows").css("grid-template-columns", "50%")
+  //   $("#Desktop").css("grid-template-columns", "50%")
   //   AppMode = "Medium"
   // }
   // if (RunningApps.length == 3) {
-  //   $("#Windows").css("grid-template-columns", "30%")
+  //   $("#Desktop").css("grid-template-columns", "30%")
   //   AppMode = "Small"
   // }
 }
 
 
-function FillWindow() {
-  if (temp = (document.getElementById("Display").className) == "DisplayNormal") {
-    document.getElementById("Display").className = "DisplayFullscreen";
-  } else {
-    document.getElementById("Drawer").className = "DrawerClosed";
-    document.getElementById("Display").className = "DisplayNormal";
+function FillWindow(temp) {
+  if (document.getElementById(temp + "WindowDecoration").style.display != "none") {
+    document.getElementById("TitleAndMenu").innerHTML = document.getElementById(temp + "WindowDecoration").innerHTML;
+    document.getElementById(temp + "WindowDecoration").style.display = "none";
+    document.getElementById(temp + "App").classList.add("AppFullScreen")
+
   }
-  AdjustWindowTiling()
+  else {
+    document.getElementById(temp + "WindowDecoration").style.display = "flex";
+    document.getElementById("TitleAndMenu").innerHTML = ''
+    document.getElementById(temp + "App").classList.remove("AppFullScreen")
+  }
+
 }
+
+
+interact('.resize-drag')
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    listeners: {
+      move(event) {
+        var target = event.target
+        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+        var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+        // update the element's style
+        target.style.width = event.rect.width + 'px'
+        target.style.height = event.rect.height + 'px'
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left
+        y += event.deltaRect.top
+
+        target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+      }
+    },
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent'
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 }
+      })
+    ],
+
+    inertia: true
+  })
+  .draggable({
+    listeners: { move: window.dragMoveListener },
+    allowFrom: '.WindowDecoration',
+    inertia: true,
+  })
 
 function MinimiseWindow(minimisingApp) {
   minimisingApp.String
+  document.getElementById("TitleAndMenu").innerHTML = ''
   temp = String("#" + minimisingApp)
   AdjustWindowTiling()
   $(temp + "App").addClass('miniApp')
   sleep(500).then(() => {
     $(temp + "App").css({ "display": "none" })
-    document.getElementById("Drawer").className = "DrawerClosed";
     document.getElementById("Dock").className = "";
-    document.getElementById("Windows").className = "";
-    document.getElementById("AllApps").className = "";
+    document.getElementById("Desktop").className = "";
     document.getElementById("Display").className = "DisplayNormal";
     $(temp + "App").removeClass('miniApp')
 
@@ -125,23 +157,6 @@ function ToggleSearch(searchApp) {
 
 function LaunchSystem(systemApp) {
   console.log(temp)
-  if (systemApp === "AppDrawer") {
-    if (temp = (document.getElementById("Drawer").className) == "DrawerClosed") {
-      document.getElementById("Drawer").className = "DrawerDrawerOpen";
-      document.getElementById("Dock").className = "DockDrawerOpen";
-      document.getElementById("Windows").className = "WindowsDrawerOpen";
-      document.getElementById("AllApps").className = "AllAppsDrawerOpen";
-
-      console.log(temp)
-    } else {
-      document.getElementById("Drawer").className = "DrawerClosed";
-      document.getElementById("Dock").className = "";
-      document.getElementById("Windows").className = "";
-      document.getElementById("AllApps").className = "";
-      console.log(temp)
-    }
-
-  }
 }
 const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -157,6 +172,7 @@ function DeepLaunch(mainapp, jumppage) {
 
 function CloseWindow(closingApp) {
   closingApp.String
+  document.getElementById("TitleAndMenu").innerHTML = ''
   temp = String("#" + closingApp);
   $(temp + "App").addClass('closeApp')
   sleep(500).then(() => {
@@ -166,7 +182,6 @@ function CloseWindow(closingApp) {
     document.getElementById("Display").className = "DisplayNormal";
     UpdateAppRunningIndicator(closingApp)
   })
-  AdjustWindowTiling()
 
 }
 function LoadPage(targetapp, newpage) {
@@ -174,7 +189,7 @@ function LoadPage(targetapp, newpage) {
   var NewPage = String(newpage);
   console.log("Loading Page")
   $('#' + TargetApp + "ScreenContainer").empty();
-  $('#' + TargetApp + "ScreenContainer").append('<embed name="' + TargetApp + 'Screen" rel="preload" style="width: 100%; height: 100%; overflow:scroll; border-radius: 0.5em; border: none;" class="Body" id="' + TargetApp + 'Screen" src="/users/' + UserSettings.Username + '/Apps/' + TargetApp + '/Screens/' + NewPage + '.html"></embed>');
+  $('#' + TargetApp + "ScreenContainer").append('<embed name="' + TargetApp + 'Screen" rel="preload" style="width: 100%; height: 100%; overflow:scroll; border-radius: 0.5em; border: none;" class="Body" id="' + TargetApp + 'Screen" src="/Apps/' + TargetApp + '/Screens/' + NewPage + '.html"></embed>');
   // temp = History.indexOf(TargetApp)
   // History[temp].push(NewPage)
   // console.log(History);
@@ -201,56 +216,64 @@ function UpdateAppRunningIndicator(app) {
 
 function LaunchWindow(launchingApp) {
   temp = String(launchingApp)
-  // $('#' + String(temp + 'AppIcon')).addClass('bounce-top');
-  // AdjustWindowTiling()
-
-  if (UserSettings.PinnedApps.includes(temp, 0) == false) {
-    $("#AppList").append('<div class="DockAppIcon" id="' + launchingApp + 'RunningAppIcon"></div>');
-    $('#' + String(launchingApp + 'RunningAppIcon')).load('/users/' + UserSettings.Username + '/Apps/' + launchingApp + '/AppIcon.html');
-  }
-
-  if (RunningApps.includes(temp, 0) == false) {
-    // AdjustWindowTiling()
-    RunningApps.push(launchingApp)
-    temp = RunningApps[RunningApps.length - 1]
-    $("#Windows").append('<div class="AppContainer openApp" id="' + launchingApp + 'App"></div>');
-    $('#' + String(temp + 'App')).load('/users/' + UserSettings.Username + '/Apps/' + temp + '/App.html');
-    $(temp + "App").removeClass("openApp");
-
-  }
-  else {
-    temp = String("#" + launchingApp);
-    $(temp + "App").addClass('unMiniApp');
-    $(temp + "App").css({ "display": "grid" })
-    sleep(500).then(() => {
-      $(temp + "App").removeClass("unMiniApp");
-      console.log(temp + " is already running");
-    })
-  }
-  // $('#' + String(temp + 'AppIcon')).removeClass('bounce-top');
-
-  document.getElementById("Drawer").className = "DrawerClosed";
-  document.getElementById("Dock").className = "";
-  document.getElementById("Windows").className = "";
-  document.getElementById("AllApps").className = "";
-  document.getElementById("Display").className = "DisplayNormal";
-  sleep(500).then(() => {
+  if (temp != "Dash") {
+    if (UserSettings.PinnedApps.includes(temp, 0) == false) {
+      $("#AppList").append('<div class="DockAppIcon" id="' + launchingApp + 'RunningAppIcon"></div>');
+      $('#' + String(launchingApp + 'RunningAppIcon')).load('/Apps/' + launchingApp + '/AppIcon.html');
+    }
+    if (RunningApps.includes(temp, 0) == false) {
+      // AdjustWindowTiling()
+      RunningApps.push(launchingApp)
+      temp = RunningApps[RunningApps.length - 1]
+      $("#Desktop").append('<div class="AppContainer resize-drag openApp" id="' + launchingApp + 'App"></div>');
+      $('#' + String(temp + 'App')).load('/Apps/' + temp + '/App.html');
+      $(temp + "App").removeClass("openApp");
+    }
+    else {
+      temp = String("#" + launchingApp);
+      $(temp + "App").addClass('unMiniApp');
+      $(temp + "App").css({ "display": "grid" })
+      sleep(500).then(() => {
+        $(temp + "App").removeClass("unMiniApp");
+        console.log(temp + " is already running");
+      })
+    }
     $(String(temp + 'App')).removeClass('openApp');
-  })
-  UpdateAppRunningIndicator(launchingApp)
+    UpdateAppRunningIndicator(launchingApp)
+  }
+
+  else {
+    Dash("Toggle")
+  }
+
+}
+
+function Dash(dashmod) {
+  if (dashmod == "Open") {
+    document.getElementById("Dash").style.display = "block"
+  }
+  else if (dashmod == "Close") {
+    document.getElementById("Dash").style.display = "none"
+  }
+  else if (dashmod == "Toggle") {
+    if (document.getElementById("Dash").style.display == "none") {
+      document.getElementById("Dash").style.display = "block"
+    }
+    else {
+      document.getElementById("Dash").style.display = "none"
+    }
+  }
 }
 
 function displayPinnedList() {
   console.log("displayAppList Triggered")
   $("#AppList").html('')
   for (let app of UserSettings.PinnedApps) {
-    $("#AppList").append('<div class="DockAppIcon" id="' + app + 'PinnedAppIcon"></div>');
-    $('#' + String(app + 'PinnedAppIcon')).load('/users/' + UserSettings.Username + '/Apps/' + app + '/AppIcon.html');
+    $("#AppList").append('<button class="DockAppIcon" onClick="LaunchWindow(`' + app + '`)" style="display: flex; align-items: middle; padding-top: 0px !importantpadding-bottom: 0px !important;background-size: cover; background-image: url(/Apps/' + app + '/AppIcon.svg)" id="' + app + 'PinnedAppIcon"></button>');
   }
   console.log("dock appended")
   for (let app of RunningApps) {
-    $("#AppList").append('<div class="DockAppIcon" id="' + app + 'RunningAppIcon"></div>');
-    $('#' + String(app + 'RunningAppIcon')).load('/users/' + UserSettings.Username + '/Apps/' + app + '/AppIcon.html');
+    $("#AppList").append('<button class="RunningAppIcon" onClick="LaunchWindow(`' + app + '`)" style="display: flex; align-items: middle; padding-top: 0px !importantpadding-bottom: 0px !important;background-size: cover; background-image: url(/Apps/' + app + '/AppIcon.svg)" id="' + app + 'PinnedAppIcon"></button>');
   }
   console.log("runningapp appended")
 }
@@ -258,8 +281,9 @@ function displayAppList(location) {
   console.log("displayAppList Triggered")
   if (location == "AllApps") {
     for (let app of UserSettings.InstalledApps) {
-      $("#" + location).append('<button class="HListItem"><div class="DockAppIcon" id="' + app + 'AllAppIcon"></div><div class="ListItemLabel">' + app + '</div></button>');
-      $('#' + String(app + 'AllAppIcon')).load('/users/' + UserSettings.Username + '/Apps/' + app + '/AppIcon.html');
+      $("#" + location).append('<div class="HListItem"><div class="DockAppIcon" id="' + app + 'AllAppIcon"></div><div class="ListItemLabel">' + app + '</div></div>');
+      temp = document.getElementById(String(app + 'AllAppIcon'));
+      temp.style.backgroundImage = String('/Apps/' + app + '/AppIcon.svg');
       // History.push(app)
     }
   }
@@ -268,15 +292,15 @@ function SettingsAppList() {
   console.log("list apps in settings")
   for (let app of UserSettings.InstalledApps) {
     console.log(app)
-    $("#SettingsHAppList:SettingsScreen").append('<button class="HListItem" onclick="parent.LoadPage(`Settings`,`Apps/`' + app + '`/Index`)"><div class="DockAppIcon" id="' + app + 'AllAppIcon"></div><div class="ListItemLabel">' + app + '</div></button>');
-    $('#' + String(app + 'AllAppIcon')).load('/users/' + UserSettings.Username + '/Apps/' + app + '/AppIcon.html');
+    $("#SettingsHAppList:SettingsScreen").append('<button class="HListItem" onclick="parent.LoadPage(`Settings`,`Apps / `' + app + '` / Index`)"><div class="DockAppIcon" id="' + app + 'AllAppIcon"></div><div class="ListItemLabel">' + app + '</div></button>');
+    $('#' + String(app + 'AllAppIcon')).load('/Apps/' + app + '/AppIcon.html');
   }
 }
 function displaySystem() {
   console.log("displaySystemList Triggered")
   for (let app of UserSettings.SystemApps) {
-    $("#System").append('<div class="DockAppIcon" id="' + app + 'PinnedAppIcon"></div>');
-    $('#' + String(app + 'PinnedAppIcon')).load('/users/' + UserSettings.Username + '/System/' + app + '/AppIcon.html');
+    $("#System").append('<div class="SystemIndicator" id="' + app + 'SystemIndicator"></div>');
+    $('#' + String(app + 'SystemIndicator')).load('/users/' + UserSettings.Username + '/System/' + app + '/SystemIndicator.html');
   }
   console.log("dock appended")
 }
